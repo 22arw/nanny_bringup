066a58c0ff9f2107d54f84b8ef7eb55c2777b9fd
slayre <colesayre15@gmail.com>
2021-02-18 21:50:09 -0600

    Adding listeners

---
+++ src/nanny_back_left_swivel_tf_ls.cpp
@@ -0,0 +1,36 @@
+#include <ros/ros.h>
+#include <geometry_msgs/PointStamped.h>
+#include <tf2_ros/transform_listener.h>
+
+int main(int argc, char** argv){
+
+    ros::init(argc, argv, "nanny_base_back_left_swivel_ls");
+
+    ros::NodeHandle node;
+
+    tf2_ros::Buffer tfBuffer;
+
+    tf2_ros::TransformListener tfListener(tfBuffer);
+
+    ros::Rate rate(10.0);
+
+    while (node.ok()){
+        geometry_msgs::TransformStamped tfStamped;
+
+        try {
+            tfBuffer.canTransform("back_left_swivel", "base_link", ros::Time(0));
+            tfStamped = tfBuffer.lookupTransform("back_left_swivel", "base_link", ros::Time(0));
+            
+        }
+        catch (tf2::LookupException &ex) {
+            ROS_WARN("%s", ex.what());
+            ros::Duration(1.0).sleep();
+
+        }
+
+        rate.sleep();
+    }
+
+    return 0;
+}
+
---
+++ src/nanny_rear_laser_tf_ls.cpp
@@ -0,0 +1,35 @@
+#include <ros/ros.h>
+#include <geometry_msgs/PointStamped.h>
+#include <tf2_ros/transform_listener.h>
+
+int main(int argc, char** argv){
+
+    ros::init(argc, argv, "nanny_base_rear_laser_ls");
+
+    ros::NodeHandle node;
+
+    tf2_ros::Buffer tfBuffer;
+
+    tf2_ros::TransformListener tfListener(tfBuffer);
+
+    ros::Rate rate(10.0);
+
+    while (node.ok()){
+        geometry_msgs::TransformStamped tfStamped;
+
+        try {
+            tfBuffer.canTransform("rear_laser", "base_link", ros::Time(0));
+            tfStamped = tfBuffer.lookupTransform("rear_laser", "base_link", ros::Time(0));
+        }
+        catch (tf2::LookupException &ex) {
+            ROS_WARN("%s", ex.what());
+            ros::Duration(1.0).sleep();
+
+        }
+
+        rate.sleep();
+    }
+
+    return 0;
+}
+
