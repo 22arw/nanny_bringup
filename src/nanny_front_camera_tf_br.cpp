342a9e5c9ee79e56c47a144e30152ee8d74136d7
Live session user <phuong.hoang7@hotmail.com>
2021-05-03 21:21:07 -0500

    added camera broadcasters and tf tree

--- src/nanny_front_camera_tf_br.cpp
+++ src/nanny_front_camera_tf_br.cpp
@@ -11,12 +11,12 @@ int main(int argc, char** argv){
 
   
   transformStamped.header.frame_id = "base_link";
-  transformStamped.child_frame_id = "rear_laser";
-  transformStamped.transform.translation.x = -0.38239;
+  transformStamped.child_frame_id = "front_camera";
+  transformStamped.transform.translation.x = 0.38239;
   transformStamped.transform.translation.y = 0;
-  transformStamped.transform.translation.z = 0.15967;
+  transformStamped.transform.translation.z = 0.25;
   tf2::Quaternion q;
-        q.setRPY(0, 0, 3.14159);
+        q.setRPY(0, 0, 0);
   transformStamped.transform.rotation.x = q.x();
   transformStamped.transform.rotation.y = q.y();
   transformStamped.transform.rotation.z = q.z();
--- src/nanny_rear_camera_tf_br.cpp
+++ src/nanny_rear_camera_tf_br.cpp
@@ -11,10 +11,10 @@ int main(int argc, char** argv){
 
   
   transformStamped.header.frame_id = "base_link";
-  transformStamped.child_frame_id = "rear_laser";
+  transformStamped.child_frame_id = "rear_camera";
   transformStamped.transform.translation.x = -0.38239;
   transformStamped.transform.translation.y = 0;
-  transformStamped.transform.translation.z = 0.15967;
+  transformStamped.transform.translation.z = 0.25;
   tf2::Quaternion q;
         q.setRPY(0, 0, 3.14159);
   transformStamped.transform.rotation.x = q.x();
